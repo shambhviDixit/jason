7d94fda9441b8ac41c9e9029011c3ae3e4d77a2d
shambhviDixit <99592580+shambhviDixit@users.noreply.github.com>
2022-03-06 16:56:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(400, 400);
+var student={
+
+  name:"priya",
+  class:7,
+  school:"jagran"
+ 
+}
+
+console.log(student.name);
+console.log(student.school);
+
+
+}
+
+function draw() {
+  background(220);
+
+
+
+
+
+
+
+
+
+
+
+}
